80f3e3cfa62a0a54eed060774c2eb8693f69f0af
Katrina de Guzman <katrinavdeguzman@gmail.com>
2017-06-14 17:37:32 -0700

    Create project

---
+++ InstaKilo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  InstaKilo
+//
+//  Created by Katrina de Guzman on 2017-06-14.
+//  Copyright © 2017 Katrina de Guzman. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
