3496abb537980635b9dee7352be951b3b26bc5e5
Katrina de Guzman <katrinavdeguzman@gmail.com>
2017-06-14 18:28:56 -0700

    Add images

--- InstaKilo/MyCollectionViewController.h
+++ InstaKilo/MyCollectionViewController.h
@@ -1,5 +1,5 @@
 //
-//  AppDelegate.h
+//  MyCollectionViewController.h
 //  InstaKilo
 //
 //  Created by Katrina de Guzman on 2017-06-14.
@@ -8,11 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate>
-
-@property (strong, nonatomic) UIWindow *window;
-
+@interface MyCollectionViewController : UICollectionViewController
 
 @end
-
-
---
+++ InstaKilo/MyCollectionViewController.m
@@ -0,0 +1,114 @@
+//
+//  MyCollectionViewController.m
+//  InstaKilo
+//
+//  Created by Katrina de Guzman on 2017-06-14.
+//  Copyright © 2017 Katrina de Guzman. All rights reserved.
+//
+
+#import "MyCollectionViewController.h"
+
+@interface MyCollectionViewController ()
+{
+    NSArray *myPhotos;
+}
+
+@end
+
+@implementation MyCollectionViewController
+
+static NSString * const reuseIdentifier = @"Cell";
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+  
+    // Initialize image array
+    myPhotos = [NSArray arrayWithObjects:@"bitmoji_0.jpg",@"bitmoji_1.jpg",@"bitmoji_2.jpg",@"bitmoji_3.jpg",@"bitmoji_4.jpg",@"bitmoji_5.jpg",@"bitmoji_6.jpg",@"bitmoji_7.jpg",@"bitmoji_8.jpg",@"bitmoji_9.jpg",@"bitmoji_10.jpg",@"bitmoji_11.jpg",@"bitmoji_12.jpg",@"bitmoji_13.jpg",@"bitmoji_14.jpg", nil];
+}
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+#pragma mark <UICollectionViewDataSource>
+
+
+
+- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView {
+#warning Incomplete implementation, return the number of sections
+    return 1;
+}
+
+
+//- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+//#warning Incomplete implementation, return the number of items
+//    return 0;
+//}
+//
+//- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+//    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:reuseIdentifier forIndexPath:indexPath];
+//    
+//    // Configure the cell
+//    
+//    return cell;
+//}
+///
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    return myPhotos.count;
+}
+
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath{
+    static NSString *identifier = @"Cell";
+    
+    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:identifier forIndexPath:indexPath];
+    
+    UIImageView *recipeImageView = (UIImageView *)[cell viewWithTag:100];
+    recipeImageView.image = [UIImage imageNamed:[myPhotos objectAtIndex:indexPath.row]];
+    
+    return cell;
+}
+
+#pragma mark <UICollectionViewDelegate>
+
+/*
+// Uncomment this method to specify if the specified item should be highlighted during tracking
+- (BOOL)collectionView:(UICollectionView *)collectionView shouldHighlightItemAtIndexPath:(NSIndexPath *)indexPath {
+	return YES;
+}
+*/
+
+/*
+// Uncomment this method to specify if the specified item should be selected
+- (BOOL)collectionView:(UICollectionView *)collectionView shouldSelectItemAtIndexPath:(NSIndexPath *)indexPath {
+    return YES;
+}
+*/
+
+/*
+// Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+- (BOOL)collectionView:(UICollectionView *)collectionView shouldShowMenuForItemAtIndexPath:(NSIndexPath *)indexPath {
+	return NO;
+}
+
+- (BOOL)collectionView:(UICollectionView *)collectionView canPerformAction:(SEL)action forItemAtIndexPath:(NSIndexPath *)indexPath withSender:(id)sender {
+	return NO;
+}
+
+- (void)collectionView:(UICollectionView *)collectionView performAction:(SEL)action forItemAtIndexPath:(NSIndexPath *)indexPath withSender:(id)sender {
+	
+}
+*/
+
+@end
